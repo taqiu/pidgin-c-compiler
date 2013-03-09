9a4bfad49babbce761d47be8d6e74042986cee8b
Tanghong Qiu <taqiu@indiana.edu>
2013-03-09 13:37:15 -0500

    update matmul.s

--- LLVM/matmul.s
+++ LLVM/matmul.s
@@ -1,67 +1,58 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;   P523:  Assignment 4
+;;   Author: Tanghong Qiu
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 define double @dotproduct(double* %A, double* %B, i32 %n) {
 entry:
-	%i = alloca i32 
-	%s = alloca double
-	store i32 0, i32* %i
-	store double 0.0, double* %s
 	br label %test
 test:
-	%i_val = load i32* %i
-	%cond = icmp slt i32 %i_val, %n
-	br i1 %cond, label %loop, label %exit
+	%i = phi i32 [0, %entry], [%i_next, %loop]
+	%s = phi double [0.000000e+00, %entry], [%s_new, %loop]
+	%cond = icmp slt i32 %i, %n
+	br i1 %cond, label %loop, label %done
 loop:
-	%A_idx = getelementptr double* %A, i32 %i_val
+	%A_idx = getelementptr double* %A, i32 %i
 	%A_val = load double* %A_idx
-	%idx_temp = mul i32 %n, %i_val
+	%idx_temp = mul i32 %n, %i
 	%B_idx = getelementptr double* %B, i32 %idx_temp
 	%B_val = load double* %B_idx
 	%product = fmul double %A_val, %B_val
-	%s_prev = load double* %s
-	%s_new = fadd double %s_prev, %product
-	store double %s_new, double* %s
+	%s_new = fadd double %s, %product
 
-	%i_current = load i32* %i	
-	%i_next = add i32 1, %i_current
-	store i32 %i_next, i32* %i
+	%i_next = add i32 %i, 1	
 	br label %test
-exit:
-	%final_s = load double* %s
-	ret double %final_s
+done:
+	ret double %s
 }
 
 define void @matmul(double* %A, double* %B, double* %C, i32 %n) {
 entry:
-	%i = alloca i32
-	%j = alloca i32
-	store i32 0, i32* %i
 	br label %outer_test
 outer_test:
-	%i_val = load i32* %i
-	%outer_cond = icmp slt i32 %i_val, %n
-	store i32 0, i32* %j
-	br i1 %outer_cond, label %test, label %exit
+	%i = phi i32 [0, %entry], [%i_next, %outer_inc]
+	%outer_cond = icmp slt i32 %i, %n
+	br i1 %outer_cond, label %test, label %done
 test:
-	%j_val = load i32* %j
-	%cond = icmp slt i32 %j_val, %n
+	%j = phi i32 [0, %outer_test], [%j_next, %loop]
+	%cond = icmp slt i32 %j, %n
 	br i1 %cond, label %loop, label %outer_inc
 loop:
-	%A_idx_temp = mul i32 %n, %i_val
+	%A_idx_temp = mul i32 %n, %i
 	%A_idx = getelementptr double* %A, i32 %A_idx_temp	
-	%B_idx = getelementptr double* %B, i32 %j_val	
+	%B_idx = getelementptr double* %B, i32 %j	
 	%retval = call double @dotproduct(double* %A_idx, double* %B_idx, i32 %n)
-	%C_idx_temp = add i32 %A_idx_temp, %j_val
+	%C_idx_temp = add i32 %A_idx_temp, %j
 	%C_idx = getelementptr double* %C, i32 %C_idx_temp
 	store double %retval, double* %C_idx
 
-	%j_current = load i32* %j	
-	%j_next = add i32 1, %j_current
-	store i32 %j_next, i32* %j
+	%j_next = add i32 1, %j
 	br label %test
 outer_inc:
-	%i_current = load i32* %i	
-	%i_next = add i32 1, %i_current
-	store i32 %i_next, i32* %i
+	%i_next = add i32 1, %i
 	br label %outer_test
-exit:
+done:
 	ret void
 }
